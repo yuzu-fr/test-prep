ae5bb9842417288b21bf0c8cb09c8e5e311796d9
jinyun0927 <62060219+jinyun0927@users.noreply.github.com>
2025-12-30 20:10:53 +0100

    fix: use hash router for github pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import Practice from '../pages/Practice.vue'
 
 const router = createRouter({
-  history: createWebHistory('/test-prep/'),
-  routes: [
-    { path: '/', component: Home },
-    { path: '/practice', component: Practice },
-  ],
-})
+    history: createWebHashHistory(),
+    routes: [
+      { path: '/', component: Home },
+      { path: '/practice', component: Practice },
+    ],
+  })
 
 export default router
